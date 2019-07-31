31f494af9f95ea6a5d7c597f156ea95b8759178b
shljshlj <34565607+shljshlj@users.noreply.github.com>
2019-07-31 02:18:46 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ## BitBook
 project
+
+
+#### Resources
+1. [DateTime manipulation](https://www.toptal.com/software/definitive-guide-to-datetime-manipulation)
+    Great article about working with `Date` object
+
+---
--- src/services/PostService.js
+++ src/services/PostService.js
@@ -3,7 +3,7 @@ import bitBookApi from '../shared/api';
 class PostService {
     async fetchPosts() {
         const response = await bitBookApi.get('./posts');
-        console.log(response);
+        console.log(response.data);
     }
 }
 
